f1e0c761d68e04837cd3a5986860d9c9af768ad6
Monkeyyy11 <gaileshd@gmail.com>
2020-01-05 10:53:52 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "no-underscore-dangle": 0,
+        "indent": 0,
+        "no-console": 0,
+        "import/newline-after-import": 0,
+        "no-await-in-loop": 0
+    }
+};
